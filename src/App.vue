cf4d2aad5f4e10c74492e30d72e7404feae03f07
Anatoliy Komarov <admin@komaroff.biz>
2022-08-25 13:09:42 +0300

    +

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,117 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-3xl text-center font-bold">Currency rate</h1>
+  <!--{{currencies}}-->
+  <!--  {{currencyList }}-->
+  <div class="container  mx-auto ">
+    <div class="flex items-start mb-4 flex-col">
+      <label class="text-xl font-bold text-left">Filter: {{ searchText }}
+        <input tupe="text" v-model="searchText" @input="currencyFilter"
+               class="w-[200px] text-base py-1 px-2 border border-1 border-blue-100">
+      </label>
+      <h2 class="text-xl font-bold text-left">Select currency:</h2>
+    </div>
+    <div class="flex flex-col items-start h-[30vh] overflow-hidden border border-1 border-gray-300 p-2 overflow-y-auto">
+      <label v-for="val in dataFilter" :key="val.Cur_Name">
+        <input type="checkbox"
+               :name="labelName"
+               :value="val.Cur_Name"
+               :checked="val.Cur_Name == value"
+               @change="getCurrencyToList(val)">
+        {{ val.Cur_Name }} - {{ val.Cur_Abbreviation }} | Code -  {{ val.Cur_Code }}
+      </label>
+    </div>
+  </div>
+
+  <GetDataFromBank :currencies="currencies"></GetDataFromBank>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GetDataFromBank from "@/components/GetDataFromBank";
+import axios from "axios";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      searchText: null,
+      labelName: 'currency',
+      selectedCurrency: null,
+      currencyBankAddress: 'https://www.nbrb.by/api/exrates/currencies',
+      currencyList: null,
+      dataFilter: null,
+      currencies: [],
+    }
+  },
+  beforeMount() {
+    if (this.currencyList == null) {
+      axios.get(this.currencyBankAddress).then((response) => {
+        this.currencyList = response.data
+        this.dataFilter = response.data
+
+      })
+    }
+
+  },
+  computed: {
+    currencyFilter() {
+      if (this.searchText) {
+        this.dataFilter = null
+        this.dataFilter = this.currencyList.filter((el) => {
+          let searchNameElement = el.Cur_Name.toLowerCase()
+          //let searchCodeElement = el.Cur_Code.toLowerCase()
+          let searchAbbrElement = el.Cur_Abbreviation.toLowerCase()
+          let inputElement = this.searchText.toLowerCase()
+          if (searchNameElement === inputElement) {
+            return el
+          }
+          // if (searchCodeElement === this.searchText.toLowerCase()) {
+          //   return el
+          // }
+          if (searchAbbrElement === this.searchText.toLowerCase()) {
+            return el
+          }
+          if (searchNameElement.includes(inputElement)) {
+            return el
+          }
+
+          // if (searchCodeElement.includes(this.searchText.toLowerCase())) {
+          //   return el
+          // }
+
+          if (searchAbbrElement.includes(this.searchText.toLowerCase())) {
+            return el
+          }
+        })
+
+      } else {
+        this.dataFilter = this.currencyList
+      }
+    }
+  },
+  methods: {
+    getCurrencyToList(val) {
+      if(!this.currencies.find(el => el.curCode === val.Cur_Code)) {
+        this.currencies.push({
+          curCode: val.Cur_Code,
+          curName: val.Cur_Name,
+          curAbbr: val.Cur_Abbreviation,
+          curScale: val.Cur_Scale
+        })
+      }else{
+       let indexOfData =  this.currencies.findIndex(el => el.curCode === val.Cur_Code)
+        this.currencies.splice(indexOfData,1)
+      }
+    }
+  },
   components: {
-    HelloWorld
+    GetDataFromBank
   }
 }
 </script>
 
-<style lang="less">
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/GetDataFromBank.vue
@@ -0,0 +1,36 @@
+<template>
+  {{ listOfCurriencies}}
+  <button @click="getList()" class="bg-blue-700 text-white py-2 px-4 rounded "> Get data from bank</button>
+  <div
+  v-for="item in data"
+  >{{item.Cur_Name}} - {{ item.Cur_Abbreviation}} - {{item.Cur_Scale}}</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "GetDataFromBank",
+  props:['currencies'],
+  data(){
+    return{
+      api: 'https://www.nbrb.by/api/exrates/currencies',
+      data: null,
+      listOfCurriencies: this.currencies,
+
+    }
+  },
+  methods: {
+    getList() {
+      axios.get(this.api).then((response) => {
+        this.data = response.data
+        console.log(response.data)
+      })
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
